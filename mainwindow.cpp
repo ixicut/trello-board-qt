9bdd92e0be181ab76809c07275f7b33622ed3284
Volodia <parikripakkabak@gmail.com>
2023-10-03 22:28:16 +0300

    updated MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,55 @@
+#include <QHBoxLayout>
+#include <QListWidget>
+#include <QLabel>
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include "customlistwidget.h"
+#include "taskcardwidget.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    QHBoxLayout *layout = new QHBoxLayout(centralWidget);
+
+    QListWidget *listWidget1 = new CustomListWidget(this);
+    QListWidget *listWidget2 = new CustomListWidget(this);
+    QListWidget *listWidget3 = new CustomListWidget(this);
+
+    TaskCardWidget* task1 = new TaskCardWidget("Task 1", "High", 123);
+    TaskCardWidget* task2 = new TaskCardWidget("Task 2", "Medium", 678);
+    TaskCardWidget* task3 = new TaskCardWidget("Task 3", "High", 234);
+    TaskCardWidget* task4 = new TaskCardWidget("Task 4", "Medium", 567);
+
+    QListWidgetItem* item1 = new QListWidgetItem(listWidget1);
+    item1->setSizeHint(task1->sizeHint());
+    listWidget1->setItemWidget(item1, task1);
+
+    QListWidgetItem* item2 = new QListWidgetItem(listWidget1);
+    item2->setSizeHint(task2->sizeHint());
+    listWidget1->setItemWidget(item2, task2);
+
+    QListWidgetItem* item3 = new QListWidgetItem(listWidget1);
+    item3->setSizeHint(task3->sizeHint());
+    listWidget1->setItemWidget(item3, task3);
+
+    QListWidgetItem* item4 = new QListWidgetItem(listWidget1);
+    item4->setSizeHint(task4->sizeHint());
+    listWidget1->setItemWidget(item4, task4);
+
+    layout->addWidget(listWidget1);
+    layout->addWidget(listWidget2);
+    layout->addWidget(listWidget3);
+
+    centralWidget->setLayout(layout);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
