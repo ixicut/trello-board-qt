3d9672b4de4e87ad9c07b9aa0d98c409e41fa3d6
Volodia <parikripakkabak@gmail.com>
2023-10-03 22:26:56 +0300

    added CustomListWidget

---
+++ customlistwidget.cpp
@@ -0,0 +1,66 @@
+#include "customlistwidget.h"
+#include <QDrag>
+#include <QDragEnterEvent>
+#include <QDataStream>
+#include <QMimeData>
+#include <QPixmap>
+#include <QDebug>
+#include "taskcardwidget.h"
+
+CustomListWidget::CustomListWidget(QWidget* parent) : QListWidget(parent) {
+    setDropIndicatorShown(false);
+    setAcceptDrops(true);
+}
+
+void CustomListWidget::dragEnterEvent(QDragEnterEvent *event)
+{
+    event->accept();
+
+    if (event->proposedAction() == Qt::MoveAction) {
+        QListWidget::dragEnterEvent(event);
+    }
+}
+
+void CustomListWidget::dropEvent(QDropEvent* event)
+{
+    const QMimeData* data = event->mimeData();
+
+    if (data->hasFormat("application/x-qabstractitemmodeldatalist")) {
+
+        QByteArray itemData = data->data("application/x-qabstractitemmodeldatalist");
+        QDataStream dataStream(&itemData, QIODevice::ReadOnly);
+
+        QString name, surname;
+        int age;
+        dataStream >> name >> surname >> age;
+
+        QString salo = data->text();
+
+        TaskCardWidget *newTaskCard = TaskCardWidget::deserializeTaskCardWidget(salo);
+
+
+        QModelIndex index = indexAt(event->pos());
+
+        int dropRow;
+
+        (index.row() == -1) ? dropRow = count() : dropRow = index.row();
+
+        QListWidgetItem* newItem = new QListWidgetItem();
+        newItem->setSizeHint(newTaskCard->sizeHint());
+        insertItem(dropRow, newItem);
+        setItemWidget(newItem, newTaskCard);
+
+        QListWidget::dropEvent(event);
+
+        qDebug() << "dropped row is" << dropRow;
+
+        QListWidget* sourceListWidget = dynamic_cast<CustomListWidget*>(event->source());
+
+        QListWidgetItem * item = sourceListWidget->currentItem();
+
+        sourceListWidget->removeItemWidget(item);
+        delete item;
+
+        sourceListWidget->setCurrentItem(newItem);
+    }
+}
---
+++ customlistwidget.h
@@ -0,0 +1,17 @@
+#ifndef CUSTOMLISTWIDGET_H
+#define CUSTOMLISTWIDGET_H
+
+#include <QListWidget>
+
+class CustomListWidget : public QListWidget
+{
+public:
+    CustomListWidget(QWidget* parent = nullptr);
+
+protected:
+    void dragEnterEvent(QDragEnterEvent* event) override;
+    void dropEvent(QDropEvent* event) override;
+
+};
+
+#endif // CUSTOMLISTWIDGET_H
